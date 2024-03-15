1bc7a71375e149072cdf1692e8029f1ed66f0aaa
cet <cets021@gmail.com>
2024-03-15 23:16:58 +0900

    :bee:

---
+++ src/main.py
@@ -0,0 +1,9 @@
+﻿from misskey import Misskey
+from pyenv import *
+
+mi = Misskey('misskey.io')
+mi.token = TOKEN2
+
+mi.notes_create('hello misskey!')
+
+# docs: https://misskeypy.readthedocs.io/ja/latest/
